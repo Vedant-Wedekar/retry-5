ec77e4678db2bb29b326f80321d0ee32eea8e97c
VEDANT WEDEKAR <vedantwedekar@gmail.com>
2024-11-30 06:57:33 +0000

    about animation by loco , gsap

--- my-app/anime-pro/src/components/About.jsx
+++ my-app/anime-pro/src/components/About.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='w-full p-20 bg-[#cdea68] rounded-tl-3xl rounded-tr-3xl text-black '>
+    // this locomotive scroll animation 
+    <div data-scroll 
+    data-scroll-section
+    // data-scroll-speed='.7'
+      className='w-full p-20 bg-[#cdea68] rounded-tl-3xl rounded-tr-3xl text-black '>
 
         <h1 className='font-["Neue_Montreal"] text-6xl leading-[3.5vw] '>Ochi is a strategic presentation agency for forward-thinking businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and hire great peo­ple.</h1>
       <div className='w-full flex gap-5 border-t-[1px]  border-[#a1b562] mt-20 pt-10'>
